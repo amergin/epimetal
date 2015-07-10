d5128a1cc34d120761f0cdd35944327f545240ca
amergin <jussi@sedaris.fi>
2015-07-11 01:35:56 +0300

    Modify som grid settings.

--- src/app/vis/som/som.js
+++ src/app/vis/som/som.js
@@ -8,76 +8,6 @@ var vis =
     'services.som',
     ]);
 
-// mod.controller('SOMBottomMenuController', ['$scope', '$templateCache', '$rootScope', 'NotifyService', 'variables', 'DatasetFactory', 'SOMService', 'PlotService', 'bottomWindowHandler', '$timeout', 'DimensionService', 'FilterService',
-//   function SOMBottomMenuController($scope, $templateCache, $rootScope, NotifyService, variables, DatasetFactory, SOMService, PlotService, bottomWindowHandler, $timeout, DimensionService, FilterService) {
-//     $scope.windowHandler = bottomWindowHandler;
-
-//     $scope.variables = variables;
-//     $scope.currentSelection = {};
-//     $scope.savedSelection = {
-//       x: SOMService.getVariables()
-//     };
-
-//     $scope.planeInput = {};
-
-//     $scope.openSettings = function() {
-//       $scope.currentSelection = angular.copy( $scope.savedSelection );
-//       var promise = NotifyService.addClosableModal( 'vis/menucomponents/som.tpl.html', $scope, {
-//         size: 'lg'
-//       });
-//       promise.then( function(res) {
-//         $scope.savedSelection = angular.copy( $scope.currentSelection );
-//         $scope.currentSelection = {};
-//         SOMService.updateVariables($scope.savedSelection.x, $scope.windowHandler);
-//       });
-//     };
-
-//     $scope.canEdit = function () {
-//       return DatasetFactory.activeSets().length > 0;
-//     };
-
-//     $scope.clear = function() {
-//       $scope.currentSelection.x = [];
-//     };
-
-//     $scope.canSubmitSOM = function () {
-//       return $scope.canEdit();
-//     };
-
-//     $scope.filterInfo = [];
-//     $scope.$watch( function() {
-//       return FilterService.getCircleFilterInfo();
-//     }, function(val) {
-//       $scope.filterInfo = val;
-//     }, true);
-
-//     $scope.canOpenPlane = function() {
-//       return SOMService.somReady();
-//     };
-
-//     $scope.saveSettings = function(selection) {
-//       if( $scope.currentSelection.x.length < 3 ) {
-//         NotifyService.addSticky('Error', 'Please select at least three variables.', 'error', { referenceId: 'som-input' });
-//         return;
-//       }
-//       NotifyService.closeModal();
-//     };
-
-//     $scope.canSubmitPlane = function(plane) {
-//       return !_.isUndefined($scope.planeInput);
-//     };
-
-//     $scope.addPlane = function(testVar) {
-//       $scope.planeInput = {};
-//       PlotService.drawSOM({ variables: { x: testVar } }, $scope.windowHandler);
-//     };
-
-
-//     var defaultVariables = ['Serum-C', 'Serum-TG', 'HDL-C', 'LDL-C', 'Glc'];
-   
-//   }
-// ]);
-
 mod.constant('SOM_DEFAULT_SIZE_X', 3);
 mod.constant('SOM_DEFAULT_SIZE_Y', 3);
 mod.constant('SOM_DEFAULT_PLANES', ['Serum-C', 'Serum-TG', 'HDL-C', 'LDL-C', 'Glc']);
@@ -118,7 +48,7 @@ mod.controller('SOMContentCtrl', ['$scope', '$rootScope', 'NotifyService', 'cont
       colWidth: 'auto',
       rowHeight: 'match',
       minSizeX: 2,
-      maxSizeX: 8,
+      maxSizeX: 20,
       minSizeY: 2,
       maxSizeY: 8,
       maxRows: 50,
