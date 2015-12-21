659f2d5052754694099c857ad4a9935739f1c0d6
amergin <jussi@sedaris.fi>
2015-12-22 01:34:12 +0200

    Rewrite link creator UI logic.

--- src/app/vis/menucomponents/linkcreator.js
+++ src/app/vis/menucomponents/linkcreator.js
@@ -1,45 +1,69 @@
 angular.module('plotter.vis.menucomponents.linkcreator', 
-  ['ngClipboard'])
+  ['ngClipboard', 'mgcrea.ngStrap.popover'])
 
 .config(function linkcreatorConfig(ngClipProvider) {
     ngClipProvider.setPath("assets/ZeroClipboard.swf");
 })
 
-.controller('LinkCreatorController', function LinkCreatorController($scope, UrlHandler, NotifyService, $state, usSpinnerService) {
+.controller('LinkCreatorController', function LinkCreatorController($scope, $q, $popover, $element, UrlHandler, NotifyService, $state, usSpinnerService) {
+
     $scope.stateLink = null;
 
-    $scope.buttonText = function() {
-      if($scope.stateLink) { return "Copy link to clipboard"; }
-      return "Create link of this view";
+    // create popover relative to the button
+    $scope.popover = $popover($element, {
+      title: 'Link this view',
+      trigger: 'manual',
+      container: 'body',
+      placement: 'bottom-right',
+      contentTemplate: 'vis/menucomponents/linkcreator.inner.tpl.html',
+      autoClose: 1
+    });
+
+    $scope.popover.$scope.getCopyLink = function() {
+      return $scope.stateLink;
     };
 
-    $scope.clicked = function() {
+    $scope.close = function() {
+      $scope.stateLink = null;
+      $scope.popover.hide();
+    };
+
+    $scope.click = function() {
+      if($scope.stateLink) { $scope.close(); }
+      else {
+        $scope.getStateLink().then(function succFn() {
+          $scope.popover.show();
+        });
+      }
+    };
+
+
+    $scope.popover.$scope.copied = function() {
       NotifyService.addTransient('Copied to clip board', 'Link copied to clip board', 'info');
-      $scope.$hide();
+      $scope.close();
     };
 
     $scope.getStateLink = function() {
-      // $scope.processing = true;
+      var defer = $q.defer();
+
       usSpinnerService.spin('linkcreator');
       UrlHandler.create()
       .then(function succFn(hash) {
         var baseUrl = window.location.origin + window.location.pathname;
         $scope.stateLink = baseUrl + $state.href($state.current.name, { state: hash }, { relative: true });
+        defer.resolve($scope.stateLink);
         // does not work:
         //$state.href($state.current.name, { state: hash }, { absolute: true });
       }, function errFn(res) {
         NotifyService.addSticky('Error', 'The current state could not be saved. Please try again.', 'error', 
           { referenceId: 'linkcreatorinfo' });
+        defer.reject('error');
       })
       .finally(function() {
-        $scope.processing = false;
-        usSpinnerService.stop('linkcreator');
       });
 
+      return defer.promise;
     };
-
-    // init on load
-    // $scope.getStateLink();
 
 })
 
