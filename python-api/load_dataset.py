0d18a6d97a7f7986936256b088d920198dcafdfa
amergin <jussi@sedaris.fi>
2016-09-24 02:45:51 +0300

    Working draft for loading MS/NMR dataset. Increase config syntax sanity checking. Correct errors in the metadata file.

--- python-api/load_dataset.py
+++ python-api/load_dataset.py
@@ -10,9 +10,7 @@ import math
 import re
 import csv
 import time
-
-LINE_SEPARATOR = "\t"
-TOPGROUP_SEPARATOR = ":"
+import copy
 
 # Utilities, not part of the class
 def _getEscapedVar(var):
@@ -35,14 +33,19 @@ def _getEscapedHeader(headerList):
 class DataLoader( object ):
 	def __init__(self, config, fileName):
 		self.cfg = Config(config)
-		self.file = fileName
+		self.dataFile = fileName
+		self.columnSeparator = self.cfg.getDataLoaderVar("column_separator")
+		self.topgroupSeparator = self.cfg.getDataLoaderVar("topgroup_separator")
 		
 		time.sleep(10)
 				
 		connect( db=self.cfg.getMongoVar('db'), alias='samples', host=self.cfg.getMongoVar('host'), port=int(self.cfg.getMongoVar('port')), w=1 )
 		connect( db=self.cfg.getMongoVar('settings_db'), alias='db_settings', host=self.cfg.getMongoVar('host'), port=int(self.cfg.getMongoVar('port')), w=1 )
 		# has 'C|' in the beginning
-		self.classVarRegex = re.compile('(C\|)(.+)', re.IGNORECASE)
+		self.classVarRegex = re.compile('(\|C\|)(.+)', re.IGNORECASE)
+		self.regVarPattern = "|regex|"
+		self.regVarRegex = re.compile(re.escape(self.regVarPattern), re.IGNORECASE)
+		self.selfRegex = re.compile('\|self\|', re.IGNORECASE)
 
 	def load(self):
 		print "[Info] Starting to load samples"
@@ -143,15 +146,25 @@ class DataLoader( object ):
 				else:
 					return sortedGroups.limit(1)[0].order + 1
 
+			def _checkDimensions(headerList, lineList, lineNo):
+				if len(headerList) != len(lineList):
+					print "[Error] Metadata file: header and line length must agree. Error at line %i." %(lineNo+1)
+					sys.exit(-1)
+
 			def _getGroup(name):
 				topGroup = None
 				groupName = name
-				if TOPGROUP_SEPARATOR in name:
-					split = name.split(TOPGROUP_SEPARATOR)
+				if self.topgroupSeparator in name:
+					split = name.split(self.topgroupSeparator)
 					topGroup = split[0].strip()
 					groupName = split[1].strip()
-				return HeaderGroup.objects.get_or_create(name=groupName, \
+				#print "getGroup=", groupName, "topgroup=", topGroup, "name=", name
+				#print "input=", groupName, "topgroup=", topGroup, "name=", name
+				res = HeaderGroup.objects.get_or_create(name=groupName, \
 					defaults={ 'name': groupName, 'topgroup': topGroup, 'order': _getNewGroupOrder() })
+				#print "getgroup result = ", res
+				return res
+
 
 			def _getSample(name):
 				return HeaderSample.objects(name=name)
@@ -165,32 +178,95 @@ class DataLoader( object ):
 				content = self.classVarRegex.search(unit).group(2)
 				return dict( keypair.split("=") for keypair in content.split("|") )
 
+			def varIsRegex(string):
+				return self.regVarRegex.search(string) is not None
 
-			fileName = self.cfg.getDataLoaderVar('header_file')
-			headerDict = dict()
+			def hasSelfRegex(string):
+				return self.selfRegex.search(string) is not None
+
+			# find the cached compiled regex by looping the recorded ones,
+			# if something is found then return the rowDict, otherwise
+			# return None
+			def getVarRegex(regexDict, string):
+				#print "searching =", string, "from dict=", regexDict
+				for matchString, payload in regexDict.iteritems():
+					result = payload.get('regex').search(string)
+					if result is not None:
+						return payload.get('row')
+				return None
+
+			metadataFileName = self.cfg.getDataLoaderVar('metadata_file')
+			separator = self.cfg.getDataLoaderVar('column_separator')
+			metaHeaderDict = dict()
+			metaHeaderRegexDict = dict()
 
 			# 1. read all metadata from file and form a dictionary from those details
 			# assume header contains: [name, desc, unit, group]
-			with open( fileName, 'r' ) as tsv:
+			with open(metadataFileName, 'r') as tsv:
 				header = []
+
+				#print "delim=", separator, type(separator), len(separator)
 				for no, line in enumerate(csv.reader(tsv, delimiter="\t")):
 					if( no is 0 ):
 						header = line
 						continue
+
+					_checkDimensions(header, line, no)
+
 					rowDict = dict(zip(header, line))
-					headerDict[rowDict.get('name')] = rowDict
-					# rewrite variable name: might contain escaped chars
-					rowDict['name'] = _getEscapedVar( rowDict.get('name') )
+					name = rowDict.get('name')
+					if varIsRegex(name):
+						raw = name.replace(self.regVarPattern, "")
+						metaHeaderRegexDict[raw] = { 
+							'regex': re.compile(raw, re.IGNORECASE),
+							'row': rowDict
+						}
+					else:
+						metaHeaderDict[name] = rowDict
+						# rewrite variable name: might contain escaped chars
+						rowDict['name'] = _getEscapedVar(name)
+					#print "metadata rowdict=", rowDict
 
 			#2. Add only variables that are actually loaded into the database from the TSV file
+			# headerList comes from the data source file
+			#print "headerList=", headerList
+			#print "metaHeaderDict=", metaHeaderDict
+			#print "metaHeaderRegexDict", metaHeaderRegexDict
 			for variable in headerList:
-				rowDict = headerDict.get(variable)
+				rowDict = metaHeaderDict.get(variable)
+				#print "var=", variable, "dict=", rowDict
+				# if variable is not found, it might be matched
+				# with one of the regex's
+				if not rowDict:
+					#print "could not for var=", variable
+					rowDict = getVarRegex(metaHeaderRegexDict, variable)
+					# no metadata for the variable, error
+					if not rowDict:
+						print "[Error] No metadata found for variable %s, exit." %variable
+						sys.exit(-1)
+					else:
+						# don't modify the original
+						rowDict = copy.deepcopy(rowDict)
+						rowDict['name'] = variable
+
+				# check if desc contains self match pattern
+				if hasSelfRegex(rowDict.get('desc')):
+					#print "var=", variable, "desc has regex"
+					rowDict['desc'] = variable
+
+				#print "rowDict=", rowDict
+				#print "after fetch=", rowDict
 				group, created = _getGroup( rowDict.get('group') )
 				samp = _getSample(rowDict.get('name'))
 				if not samp:
-					payload = rowDict
+					# account for the case the group is matched more than
+					# once due to regex. Make no modifications to the original
+					# metadata payload so it can be reused in further iterations.
+					# That is, make a copy of the original.
+					payload = copy.deepcopy(rowDict)
 					if _isClassVariable(payload):
 						payload['classed'] = True
+						#print "payload=", payload
 						payload['unit'] = getSplitClassVariable(payload)
 
 					payload['group'] = group
@@ -225,15 +301,15 @@ class DataLoader( object ):
 				print '[Error] Header and line lengths must agree, error at line %i' %lineNo
 				sys.exit(-1)
 
-		with open( self.file ) as fi:
+		with open(self.dataFile) as fi:
 			header = None
 			datasetKey = self.cfg.getDataLoaderVar('dataset_identifier')
-			defaultDatasetName = self.cfg.getDataLoaderVar('dataset_default')
 			sampleidKey = self.cfg.getDataLoaderVar('sampleid_identifier')
+			defaultDatasetName = self.cfg.getDataLoaderVar('dataset_default')
 			for lineNo, line in enumerate(fi):
 				if( lineNo == 0):
 					# header
-					header = line.strip().split(LINE_SEPARATOR)
+					header = line.strip().split("\t")#self.columnSeparator)
 					self._modifyHeader(header)
 					header = _getEscapedHeader(header)
 					continue
@@ -241,25 +317,30 @@ class DataLoader( object ):
 				line = line.strip()
 
 				if len(line) is 0:
+					print "[Info] Line number %i is empty." %lineNo
 					# empty line, silently continue
 					continue
 
 				# normal line
-				values = line.strip().split(LINE_SEPARATOR)
+				values = line.strip().split("\t")#self.columnSeparator)
 
 				valuesDict = dict( zip( header, values ) )
 
 				dataset = valuesDict.get(datasetKey, defaultDatasetName)
+
+				# check every column is provided on this line
 				checkHeaderAndSampleDimensions(header, valuesDict, lineNo)
 
 				variables = {}
 				for key, value in valuesDict.iteritems():
+					# dataset and samplename or not numeric; skip
 					if key == datasetKey or key == sampleidKey:
 						continue
 					try:
 						variables[key.encode('ascii')] = ffloat( float(valuesDict[key]) )
 					except ValueError:
 						variables[key.encode('ascii')] = ffloat( float('NaN') )
+				#print "values=", valuesDict, sampleidKey
 				sample = Sample(dataset=dataset, sampleid=valuesDict[sampleidKey], variables=variables)
 				sample.save()
 
