d2e6ae9483fd9cef2b9b3a3d8ec1c23ed161fed0
amergin <jussi@sedaris.fi>
2016-10-04 14:49:24 +0300

    Flush settings and SOM objects as well.

--- python-api/flush_database.py
+++ python-api/flush_database.py
@@ -10,16 +10,26 @@ KEYBOARD_REGEX = '(C\|)(?:.+)'
 class Flusher(object):
 	def __init__(self, config):
 		self.cfg = Config(config)
-		self.client = connect( db=self.cfg.getMongoVar('db'), host=self.cfg.getMongoVar('host'), port=int(self.cfg.getMongoVar('port')), w=1 )
-		self.db = _get_db()
+		self.client = connect(db=self.cfg.getMongoVar('db'), host=self.cfg.getMongoVar('host'), port=int(self.cfg.getMongoVar('port')), w=1)
+		print self.client
 
-	def getCollections(self):
-		return self.db.collection_names(include_system_collections=False)
+	def getSampleCollections(self):
+		db = self.client[self.cfg.getMongoVar('db')]
+		return db.collection_names(include_system_collections=False)
+
+	def getSOMCollections(self):
+		db = self.client[self.cfg.getMongoVar('somdb')]
+		return db.collection_names(include_system_collections=False)
+
+	def getSettingsCollections(self):
+		db = self.client[self.cfg.getMongoVar('settings_db')]
+		return db.collection_names(include_system_collections=False)
 
 	def flush(self):
 		self.client.drop_database(self.cfg.getMongoVar('db'))
-		print "Flush complete"
-
+		self.client.drop_database(self.cfg.getMongoVar('somdb'))
+		self.client.drop_database(self.cfg.getMongoVar('settings_db'))
+		print "[Info] Flush complete"
 
 def main():
 
@@ -44,11 +54,16 @@ def main():
 		print "[Error] could not read configuration file. EXIT"
 		sys.exit(-1)
 
-	print "Warning: this utility will drop the database defined in %s" %configFile
+	print "Warning: this utility will drop the databases defined in %s" %configFile
 	
 	flusher = Flusher(configFile)
-	collections = flusher.getCollections()
-	print "[Info] Collections currently in the database: %s" % ", ".join(collections)
+	sampleCollections = flusher.getSampleCollections()
+	somCollections = flusher.getSOMCollections()
+	settingsCollections = flusher.getSettingsCollections()
+
+	print "[Info] Collections currently in the sample database: %s" % ", ".join(sampleCollections)
+	print "[Info] Collections currently in the SOM database: %s" % ", ".join(somCollections)
+	print "[Info] Collections currently in the settings database: %s" % ", ".join(settingsCollections)
 
 	regex = re.compile('(yes)', re.IGNORECASE)
 
