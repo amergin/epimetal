877e8b28a1a5480334a12bb32562f3ab4262a6c1
Antti Kangas <antti.kangas@protonmail.com>
2019-09-09 21:53:54 +0300

    Making grunt watch and compile also utilities.regression.js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -471,7 +471,7 @@ module.exports = function ( grunt ) {
         files: [ 
           '<%= app_files.js %>'
         ],
-        tasks: [ 'jshint:src', 'karma:unit:run', 'copy:build_appjs' ]
+        tasks: [ 'jshint:src', 'karma:unit:run', 'copy:build_appjs', 'copy:build_vendor_assets' ]
       },
 
       /**
